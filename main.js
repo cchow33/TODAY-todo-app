530c93784a22cf0ab1db753dc4085daba62e9a03
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-09-12 18:29:09 -0400

    Update app.js

--- main.js
+++ main.js
@@ -1,80 +1,38 @@
-// Select DOM
+// Select DOM elements
 const addBtn = document.querySelector('.add-btn');
-const deleteBtn = document.querySelector('.trash-btn');
 const input = document.getElementById('input');
-const todoList = document.querySelector(".todo-list");
+const list = document.querySelector('.todo-list');
 const clearAll = document.getElementById('clearAll');
-const sort = document.getElementById('sort');
 
-// Event Listeners
-// document.addEventListener("DOMContentLoaded", getTodos);
-addBtn.addEventListener("click", addTodo);
-todoList.addEventListener('click', deleteCheck);
-// deleteBtn.addEventListener("click", deleteTodo);
-
-// Functions
-
-// (1) Add new todo task:
-function addTodo(event) {
-    console.log('Adding item')
-    // Prevent browser from refreshing/form from submitting
-    event.preventDefault();
-    // Create a div called 'todoDiv'
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-        // Create Checked Button
-        const completedButton = document.createElement("button");
-        completedButton.innerHTML = `<i class="fa fa-check"></i>`;
-        completedButton.classList.add("complete-btn");
-        todoDiv.appendChild(completedButton);
-        // Create a list called 'newTodo'
-        const newTodo = document.createElement("li");
-        newTodo.innerText = input.value;
-        // Save to local - do this last
-        // Save to local
-        // saveLocalTodos(input.value);
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild(newTodo);
-        input.value = "";
-        // Create delete trashcan button
-        const trashButton = document.createElement("button");
-        trashButton.innerHTML = `<i class="fa fa-trash"></i>`;
-        trashButton.classList.add("trash-btn");
-        todoDiv.appendChild(trashButton);
-    //attach final Todo
-    todoList.appendChild(todoDiv);
-  }
-
-// (2) Delete a todo task:
-function deleteCheck(e) {
-    const item = e.target;
-    console.log(e.target);
-
-    if (item.classList[0] === "trash-btn") {
-        console.log('deleting item')
-        const todo = item.parentElement;
-        todo.remove();
+addBtn.addEventListener('click', (e) => {
+    if(input.value != ''){
+        // Prevent browser from refreshing/form from submitting
+        e.preventDefault();
+        // Create a new task ('li')
+        const task = document.createElement('li');
+        task.innerHTML = input.value;
+        list.appendChild(task);
+        // Create delete button ('span')
+        const remove = document.createElement('span');
+        remove.innerHTML = '✓';
+        task.appendChild(remove);
     }
+    // Delete a task when user clicks '✓'
+    const close = document.querySelectorAll('span');
+    for (let i = 0; i < close.length; i++){
+        close[i].addEventListener('click', () => {
+            close[i].parentElement.style.opacity = 0;
+            setTimeout(() => {
+                close[i].parentElement.style.display = 'none';
+            }, 500);
+        })
+    }
+    input.value = '';
 
-        // // e.target.parentElement.remove();
-        // const todo = .parentElement;
-        // // todo.classList.add("fall");
-        // // //at the end
-        // removeLocalTodos(todo);
-        // todo.addEventListener("transitionend", e => {
-        // todo.remove();
-        // });
+    // Delete all tasksd
+    clearAll.addEventListener('click', (e) => {
+        console.log('Delete all tasks')
+        list.innerHTML = ''; 
+    })
+});
 
-    if (item.classList[0] === "complete-btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completed");
-        console.log(todo);
-    
-}
-
-// Clear all tasks
-clearAll.onclick = function clearTasks(){
-    console.log('Delete all tasks')
-    list.innerHTML = '';
-}
-}

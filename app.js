882fdba430edba66ed38cb3895a7c772ad22a769
Carmen Chow <carmendevelops@gmail.com>
2024-03-15 20:21:00 -0400

    add jQuery CDN and remove duplicate Google Font imports

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
 // Select DOM elements
-const addBtn = document.querySelector('.add-btn');
-const input = document.getElementById('input');
-const list = document.querySelector('.todo-list');
-const clearAll = document.getElementById('clearAll');
+const addBtn = document.querySelector(".add-btn");
+// const addBtn = document.querySelector(".add-btn");
+const input = document.getElementById("input");
+// const input = document.getElementById("input");
+const list = document.querySelector(".todo-list");
+// const list = document.querySelector(".todo-list");
+const clearAll = document.getElementById("clearAll");
+// const clearAll = document.getElementById("clearAll");
 
-addBtn.addEventListener('click', (e) => {
-    if(input.value != ''){
-        // Prevent browser from refreshing/form from submitting
-        e.preventDefault();
-        // Create a new task ('li')
-        const task = document.createElement('li');
-        task.innerHTML = input.value;
-        list.appendChild(task);
-        // Create delete button ('span')
-        const remove = document.createElement('span');
-        remove.innerHTML = '✓';
-        task.appendChild(remove);
-    }
-    // Delete a task when user clicks '✓'
-    const close = document.querySelectorAll('span');
-    for (let i = 0; i < close.length; i++){
-        close[i].addEventListener('click', () => {
-            close[i].parentElement.style.opacity = 0;
-            setTimeout(() => {
-                close[i].parentElement.style.display = 'none';
-            }, 600);
-        })
-    }
-    input.value = '';
+addBtn.addEventListener("click", (e) => {
+  if (input.value != "") {
+    e.preventDefault();
+    const task = document.createElement("li");
+    task.innerHTML = input.value;
+    list.appendChild(task);
+    // Create delete button ('span')
+    const removeBtn = document.createElement("span");
+    removeBtn.innerHTML = "✓";
+    task.appendChild(removeBtn);
+  }
+  // Delete a task when user clicks '✓'
+  const closeBtn = document.querySelectorAll("span");
+  for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener("click", () => {
+      closeBtn[i].parentElement.style.opacity = 0;
+      setTimeout(() => {
+        closeBtn[i].parentElement.style.display = "none";
+      }, 600);
+    });
+  }
 
-    // Delete all tasksd
-    clearAll.addEventListener('click', (e) => {
-        console.log('Delete all tasks')
-        list.innerHTML = ''; 
-    })
+  input.value = "";
+
+  // Delete all tasksd
+  clearAll.addEventListener("click", (e) => {
+    console.log("Delete all tasks");
+    list.innerHTML = "";
+  });
 });
